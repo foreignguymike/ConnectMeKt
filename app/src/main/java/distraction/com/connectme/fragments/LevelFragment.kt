2c4dc95c2e846993cb9f5c3a8f8da21d26159894
Mike S <foreign.guy.mike@gmail.com>
2018-07-30 17:11:53 -0400

    solving and stuff

--- app/src/main/java/distraction/com/connectme/fragments/LevelFragment.kt
+++ app/src/main/java/distraction/com/connectme/fragments/LevelFragment.kt
@@ -1,14 +1,13 @@
 package distraction.com.connectme.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import distraction.com.connectme.R
 import distraction.com.connectme.data.LevelData
-import distraction.com.connectme.utils.forEach
-import distraction.com.connectme.utils.getColorCompat
-import distraction.com.connectme.utils.setBackgroundTint
+import distraction.com.connectme.utils.*
 import distraction.com.connectme.views.GridListener
 import kotlinx.android.synthetic.main.fragment_level.*
 
@@ -18,6 +17,8 @@ class LevelFragment : BaseFragment(), GridListener {
     private val data by lazy<LevelData> {
         arguments.let { it.getParcelable(KEY_DATA) }
     }
+
+    private var moves: Int = 0
 
     companion object {
         fun newInstance(data: LevelData) = LevelFragment().apply {
@@ -35,17 +36,29 @@ class LevelFragment : BaseFragment(), GridListener {
         headerTextView.text = resources.getString(R.string.level_number, data.level)
         targetTextView.text = resources.getString(R.string.target_number, data.target.toString())
         bestTextView.text = resources.getString(R.string.best_number, if (data.best > 0) data.best.toString() else "-")
-        movesTextView.text = resources.getString(R.string.moves_number, 0.toString())
+        movesTextView.text = resources.getString(R.string.moves_number, moves.toString())
 
         forEach(targetTextView, bestTextView, movesTextView, grid) {
             it.setBackgroundTint(context.getColorCompat(R.color.colorPrimary))
         }
 
-        grid.createGrid(data.numRows, data.numCols, data.grid)
+        refreshButton.setOnClickListener {
+            init()
+        }
+
+        init()
         grid.setGridListener(this)
     }
 
     override fun onMove(grid: IntArray) {
+        movesTextView.text = resources.getString(R.string.moves_number, (++moves).toString())
+        val solved = solved(to2D(grid, data.numRows, data.numCols), 3)
+        Log.e("TAG", "solved = $solved")
+    }
 
+    fun init() {
+        grid.createGrid(data.numRows, data.numCols, data.grid)
+        moves = 0
+        movesTextView.text = resources.getString(R.string.moves_number, (moves).toString())
     }
 }
--- app/src/main/java/distraction/com/connectme/utils/Utils.kt
+++ app/src/main/java/distraction/com/connectme/utils/Utils.kt
@@ -10,6 +10,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.content.ContextCompat
 import android.view.View
 import distraction.com.connectme.R
+import java.util.*
 
 fun FragmentManager.transaction(layout: Int, f: Fragment, tag: String = f.javaClass.simpleName, backstack: Boolean = true, animate: Boolean = true) {
     beginTransaction()
@@ -44,3 +45,72 @@ fun Context.getColorCompat(@ColorRes color: Int) = ContextCompat.getColor(this, 
 fun Activity.getScreenSize() = Point().apply { windowManager.defaultDisplay.getSize(this) }
 
 inline fun <T> forEach(vararg elements: T, func: (T) -> Unit) = elements.forEach { func(it) }
+
+fun to2D(arr: IntArray, numRows: Int, numCols: Int): Array<IntArray> {
+    val grid = Array(numRows) { IntArray(numCols) }
+    arr.forEachIndexed { index, it ->
+        val row = index / numCols
+        val col = index % numCols
+        grid[row][col] = it
+    }
+    return grid
+}
+
+fun solved(grid: Array<IntArray>, range: Int):Boolean {
+    val numRows = grid.size
+    val numCols = grid[0].size
+    val visited = Array(numRows) { BooleanArray(numCols) }
+    val checkedColors = BooleanArray(range)
+
+    for (row in 0 until numRows) {
+        for (col in 0 until numCols) {
+            val type = grid[row][col]
+            if (checkedColors[type] && !visited[row][col]) {
+                return false
+            }
+            checkedColors[type] = true
+            bfs(grid, visited, row, col)
+        }
+    }
+    return true
+}
+
+private fun bfs(grid: Array<IntArray>, visited: Array<BooleanArray>, r: Int, c: Int) {
+    var row = r
+    var col = c
+    val numRows = grid.size
+    val numCols = grid[0].size
+    val queue = LinkedList<Point>()
+    queue.add(Point(row, col))
+    visited[row][col] = true
+    while (!queue.isEmpty()) {
+        val currentCell = queue.poll()
+        row = currentCell.x
+        col = currentCell.y
+        val `val` = grid[row][col]
+        if (col > 0) {
+            if (!visited[row][col - 1] && grid[row][col - 1] == `val`) {
+                queue.add(Point(row, col - 1))
+                visited[row][col - 1] = true
+            }
+        }
+        if (col < numCols - 1 && grid[row][col + 1] == `val`) {
+            if (!visited[row][col + 1]) {
+                queue.add(Point(row, col + 1))
+                visited[row][col + 1] = true
+            }
+        }
+        if (row > 0 && grid[row - 1][col] == `val`) {
+            if (!visited[row - 1][col]) {
+                queue.add(Point(row - 1, col))
+                visited[row - 1][col] = true
+            }
+        }
+        if (row < numRows - 1 && grid[row + 1][col] == `val`) {
+            if (!visited[row + 1][col]) {
+                queue.add(Point(row + 1, col))
+                visited[row + 1][col] = true
+            }
+        }
+    }
+}
